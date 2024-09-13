5ae27af9addc0a6ed618a84b2a62f87331c58073
charendra910 <charendra910@rku.ac.in>
2024-09-13 09:16:26 +0530

    Add project files.

---
+++ Q1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Solved_Questions
+{
+    internal class Q1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Harendra Chaudhary");
+        }
+
+    }
+}
